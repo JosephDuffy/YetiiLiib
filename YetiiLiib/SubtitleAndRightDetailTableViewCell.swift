18d35547a61a9ff3fad3b3468f71fe088254814b
Joseph Duffy <josephduffy@me.com>
2018-01-06 02:45:15 +0000

    Fixes runtime warning about layout of `SubtitleAndRightDetailTableViewCell`

--- YetiiLiib/SubtitleAndRightDetailTableViewCell.swift
+++ YetiiLiib/SubtitleAndRightDetailTableViewCell.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 public class SubtitleAndRightDetailTableViewCell: UITableViewCell {
+
     public class func reuseIdentifier() -> String {
         return "SubtitleAndRightDetailTableViewCell"
     }
@@ -18,24 +19,32 @@ public class SubtitleAndRightDetailTableViewCell: UITableViewCell {
     private var didUpdateConstraints = false
 
     public override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
-        self.rightDetailLabel = UILabel()
+        rightDetailLabel = UILabel()
 
         super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
 
-        self.rightDetailLabel.translatesAutoresizingMaskIntoConstraints = false
+        rightDetailLabel.translatesAutoresizingMaskIntoConstraints = false
 
-        self.contentView.addSubview(rightDetailLabel)
+        contentView.addSubview(rightDetailLabel)
 
-        self.contentView.addConstraints([
-            NSLayoutConstraint(item: self.rightDetailLabel, attribute: .trailingMargin, relatedBy: .equal, toItem: self.contentView, attribute: .trailingMargin, multiplier: 1, constant: -8),
-            NSLayoutConstraint(item: self.rightDetailLabel, attribute: .centerY, relatedBy: .equal, toItem: self.contentView, attribute: .centerY, multiplier: 1, constant: 0),
-            NSLayoutConstraint(item: self.rightDetailLabel, attribute: .leading, relatedBy: .greaterThanOrEqual, toItem: self.textLabel, attribute: .trailing, multiplier: 1, constant: 8),
-            NSLayoutConstraint(item: self.rightDetailLabel, attribute: .leading, relatedBy: .greaterThanOrEqual, toItem: self.detailTextLabel, attribute: .trailing, multiplier: 1, constant: 8)
+        let topConstraint = NSLayoutConstraint(item: rightDetailLabel, attribute: .top, relatedBy: .equal, toItem: contentView, attribute: .top, multiplier: 1, constant: 18)
+        let bottomConstraint = NSLayoutConstraint(item: rightDetailLabel, attribute: .bottom, relatedBy: .equal, toItem: contentView, attribute: .bottom, multiplier: 1, constant: 18)
+        topConstraint.priority = .defaultHigh
+        bottomConstraint.priority = .defaultHigh
+
+        contentView.addConstraints([
+            NSLayoutConstraint(item: rightDetailLabel, attribute: .trailingMargin, relatedBy: .equal, toItem: contentView, attribute: .trailingMargin, multiplier: 1, constant: -8),
+            NSLayoutConstraint(item: rightDetailLabel, attribute: .centerY, relatedBy: .equal, toItem: contentView, attribute: .centerY, multiplier: 1, constant: 0),
+            NSLayoutConstraint(item: rightDetailLabel, attribute: .leading, relatedBy: .greaterThanOrEqual, toItem: textLabel, attribute: .trailing, multiplier: 1, constant: 8),
+            NSLayoutConstraint(item: rightDetailLabel, attribute: .leading, relatedBy: .greaterThanOrEqual, toItem: detailTextLabel, attribute: .trailing, multiplier: 1, constant: 8),
+            NSLayoutConstraint(item: rightDetailLabel, attribute: .top, relatedBy: .greaterThanOrEqual, toItem: contentView, attribute: .top, multiplier: 1, constant: 18),
+            NSLayoutConstraint(item: rightDetailLabel, attribute: .bottom, relatedBy: .lessThanOrEqual, toItem: contentView, attribute: .bottom, multiplier: 1, constant: 18),
         ])
     }
-    
+
     public required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
 }
+
